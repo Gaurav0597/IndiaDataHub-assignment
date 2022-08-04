c374b2e93ddd7eb4b59c65d3fd08c03ff5581ee2
GauravPetkar <gauravpetkar28@gmail.com>
2022-08-04 21:33:29 +0530

    React App Created

---
+++ assignment/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  const [result,setResult]= useState("")
+
+  return (
+    <div className="container">
+     
+    </div>
+  );
+}
+
+export default App;
